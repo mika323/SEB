0f75ca36ab16e574c0225c166c0237e56f334865
Шаститко Милана <Shastitko_MD_25@student.grsu.by>
2025-12-06 13:31:12 +0300

    реализовала функцию load_users_data()

---
+++ Lab_6/2.1.py
@@ -0,0 +1,19 @@
+import xml.etree.ElementTree as ET
+def load_users_data():
+    try:
+        users_tree = ET.parse('users.xml')
+        users = []
+        for user_elem in users_tree.getroot().findall('user'):
+            user = {
+                'user_id': int(user_elem.find('user_id').text),
+                'name': user_elem.find('name').text,
+                'age': int(user_elem.find('age').text),
+                'weight': int(user_elem.find('weight').text),
+                'fitness_level': user_elem.find('fitness_level').text,
+                'workouts': []
+            }
+            users.append(user)
+        return users
+    except FileNotFoundError:
+        print("Файл не найден")
+        return []
