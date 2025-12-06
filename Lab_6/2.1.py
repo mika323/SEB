3b20f15ba6378464ffa7af818c3baac5cdd6beae
Шаститко Милана <Shastitko_MD_25@student.grsu.by>
2025-12-06 13:53:04 +0300

    реализовала функцию load_workouts_data()

--- Lab_6/2.1.py
+++ Lab_6/2.1.py
@@ -16,4 +16,27 @@ def load_users_data():
         return users
     except FileNotFoundError:
         print("Файл не найден")
+
+        return []
+
+def  load_workouts_data():
+    try:
+        workouts_tree = ET.parse('workouts.xml')
+        workouts = []
+        for workout_elem in workouts_tree.getroot().findall('workout'):
+            workout = {
+                'workout_id': int(workout_elem.find('workout_id').text),
+                'user_id' : int(workout_elem.find('user_id').text),
+                'date': workout_elem.find('date').text,
+                'type': workout_elem.find('type').text,
+                'duration': int(workout_elem.find('duration').text),
+                'distance': float(workout_elem.find('distance').text),
+                'calories': int(workout_elem.find('calories').text),
+                'avg_heart': int(workout_elem.find('avg_heart').text),
+                'intensity': workout_elem.find('intensity').text
+            }
+            workouts.append(workout)
+        return workouts
+    except FileNotFoundError:
+        print("Файл не найден")
         return []
